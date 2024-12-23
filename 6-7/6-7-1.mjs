a415e0f5270febea60dfbe1033d9dc070bae5561
hirong <nkhshc421037@g.neec.ac.jp>
2024-12-23 15:44:59 +0900

    課題6-7

---
+++ 6-7/6-7-1.mjs
@@ -0,0 +1,3 @@
+const data = [1, 2, 1, 3, 4]
+const dataset = new Set(data)
+console.log(dataset)
---
+++ 6-7/6-7-2.mjs
@@ -0,0 +1,6 @@
+const data = [1, 2, 3, 3, 4]
+const dataset = new Set(data)
+for(const value of dataset)
+{
+    console.log(value)
+}
