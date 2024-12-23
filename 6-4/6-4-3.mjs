f0a358ae40057ea9385a3e9ca6f35ec9761f2ec0
hirong <nkhshc421037@g.neec.ac.jp>
2024-12-23 14:19:47 +0900

    課題6-4

---
+++ 6-4/6-4-3.mjs
@@ -0,0 +1,17 @@
+const score = 70
+if(score >= 90)
+{
+    console.log("Excellent")
+}
+else if(score >= 80)
+{
+    console.log("Good")
+}
+else if(score >= 60)
+{
+    console.log("Normal")
+}
+else
+{
+    console.log("Red")
+}
---
+++ 6-4/6-4-4.mjs
@@ -0,0 +1,9 @@
+const value = 15
+if(value%3 == 0)
+{
+    console.log(value + "は3の倍数")
+}
+else
+{
+    console.log(value + "は3の倍数でない")
+}
